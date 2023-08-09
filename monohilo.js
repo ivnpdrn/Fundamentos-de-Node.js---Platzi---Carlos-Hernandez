85b74f7d8636dd337b56b2b37e4ac189ae91e28b
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-09 12:45:02 -0400

    Issue creado por una variable no definida. No lo descubrio el compilador. Solo al correr.

---
+++ monohilo.js
@@ -0,0 +1,19 @@
+console.log('Hola mundo');
+
+// setInterval(function()  {
+//    console.log('Sigo activo');
+//}, 1000);
+
+let i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+
+//    if (i === 5)   {
+//        var a= 3 + z;
+//    }
+}, 1000);  // se ejecuta cuando se alcance el tiempo definido, en este caso 1000 mili segundos
+
+console.log('Segunda instruccion');
+
+
