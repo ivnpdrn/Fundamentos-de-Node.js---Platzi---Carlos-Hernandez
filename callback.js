5fb4de737a362b80fc2d7add8d2c3dd4bff54c67
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-09 17:00:22 -0400

    two functions - two callbacks - nested

--- callback.js
+++ callback.js
@@ -6,10 +6,21 @@ function hola(nombre, miCallback)  {
     }, 1000);
 }
 
-    console.log('Iniciando proceso...');
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Adios ', nombre);
+        otroCallback();
+    }, 1000);
+    
+}    
 
+    console.log('Iniciando proceso...');
     hola('Carlos', function() {
-        console.log('Terminando proceso...');
+        adios('Carlos', function() {
+            console.log('Terminando proceso...');
+
+        });
+     
     });
 
         
