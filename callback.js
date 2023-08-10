93445fe767cd6db6860bc2f8721df32adad3f9ed
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-09 21:42:29 -0400

    el 1er argumento de instancia se usa por f()
    el 2do argumento de la instancia espera por el retorno de la funcion invocada.
    y se ejecuta de acuerdo a sus elementos.

--- callback.js
+++ callback.js
@@ -1,26 +1,18 @@
-function hola(nombre, miCallback)  {
-    setTimeout(function()  {
-        console.log('Hola, '+ nombre);
-        miCallback(nombre);
-    }, 1500);
-}
 
-function adios(nombre, otroCallback) {
-    setTimeout(function() {
-        console.log('Adios ', nombre);
-        otroCallback();
-    }, 1000);
-    
-}    
+
+
+function hola(nombre, miCallback)  {setTimeout(function()  {console.log('Hola, '+ nombre); miCallback(nombre);}, 1500);}
+
+// despues de esperar por el tiempo de setTimeout, se ejecuta la primera funcion y despues la segunda que en este caso es un Callback que se devuelve con el nombre a la instancia que le llamo, para ejecutar la funcion en el segundo argumento, el que espera los callback. 
+
+function adios(nombre, otroCallback) {setTimeout(function() {console.log('Adios ', nombre); otroCallback();}, 1000);} 
+
+
 
     console.log('Iniciando proceso...');
-    hola('Carlos', function(nombre) {     // ese nombre viene con el callback
-        adios(nombre, function() {
-            console.log('Terminando proceso...');
-    
-        });
-     
-    });
+    hola('Carlos', function(nombre) { adios(nombre, function() { console.log('Terminando proceso...');});});
+
+    // primer argumento que llama la funcion es usado por esta. El segundo argumento esperara la ejecucion de la funcion llamada y se ejecutara si es una funcion de acuerdo al valor recibido. 
 
     //hola('Carlos', function () {});
     //adios('Carlos', function () {});
