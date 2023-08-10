e81dead047d17f9c611a9d2a6d7667eb492fc99b
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-10 14:24:46 -0400

    funcion mas compleja, simplifica los callbacks

--- callbackHell.js
+++ callbackHell.js
@@ -9,11 +9,16 @@ function hablar(callbackHablar) {setTimeout(function() {console.log('Bla bla bla
 
 function adios(nombre, otroCallback) {setTimeout(function() {console.log('Adios ', nombre); otroCallback();}, 1000);} 
 
+function conversacion(nombre, veces, callback) {if (veces > 0) {hablar(function () { conversacion (nombre, --veces, callback);})} else { adios(nombre, callback);}}
+
+
+
 
 // --
 
     console.log('Iniciando proceso...');
+    hola('Carlos', function (nombre) {conversacion(nombre,3,function() {console.log('Proceso terminado');});});
 
-    hola('Carlos', function(nombre) {adios(nombre,function() { console.log('Terminamos');});});
+// last active line:    hola('Carlos', function(nombre) {adios(nombre,function() { console.log('Terminamos');});});
 
     // hola('Carlos', function(nombre) { hablar(function() { hablar(function() { hablar(function() { adios(nombre, function() { console.log('Terminando proceso...');});});});});});
