bbcf142063bb238571a100de62243f342e88ba24
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-11 15:14:11 -0400

    cleaning the code

--- promises.js
+++ promises.js
@@ -1,16 +1,12 @@
 
-function hola(nombre)  {
-    return new Promise(function (resolve, reject) { setTimeout(function() {console.log('Hola, '+ nombre); resolve(nombre);}, 1500);});
+function hola(nombre)  { return new Promise(function (resolve, reject) { setTimeout(function() {console.log('Hola, '+ nombre); resolve(nombre);}, 1500);});
     
     
     setTimeout(function()  {console.log('Hola, '+ nombre); miCallback(nombre);}, 1500);}
 
 
 
-function hablar(nombre) { return new Promise (( resolve, reject) => { setTimeout(function() {console.log('Bla bla bla bla...'); 
-// resolve(nombre);
-reject('Hay un error');
-}, 1000); }); }
+function hablar(nombre) { return new Promise (( resolve, reject) => { setTimeout(function() {console.log('Bla bla bla bla...');resolve();}, 1000); }); }
 
 
 
@@ -28,12 +24,12 @@ hola('Carlos')
     .then(hablar)
     .then(hablar)
     .then(adios)
-    .then((nombre) => {console.log('Terminado el proceso');})
+    .then((nombre) => {console.log('Terminado el proceso')})
 
-    .catch(error => {
-        console.error('Ha habido un error:');
-        console.error(error);
-
-    }) // catch: asegurandose que todo lo que pasa aqui no falla, y en caso de que falle console.error('Ha habido un error:')
+    //.catch(error => {
+    //   console.error('Ha habido un error:');
+    //    console.error(error); })
+        
+    // catch: asegurandose que todo lo que pasa aqui no falla, y en caso de que falle console.error('Ha habido un error:')
 
     // siempre que se use .then entonces usa .catch
