a1bdb52e7e419eb4e9bcea44343f591c74f2fcc7
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-11 15:32:12 -0400

    cleaned version

--- promises.js
+++ promises.js
@@ -1,18 +1,13 @@
 
-function hola(nombre)  { return new Promise(function (resolve, reject) { setTimeout(function() {console.log('Hola, '+ nombre); resolve(nombre);}, 1500);});
+function hola(nombre)  { return new Promise((resolve, reject) => { setTimeout(function() {console.log('Hola, '+ nombre); resolve(nombre);}, 1500);});}
     
-    
-    setTimeout(function()  {console.log('Hola, '+ nombre); miCallback(nombre);}, 1500);}
+        
 
-
-
-function hablar(nombre) { return new Promise (( resolve, reject) => { setTimeout(function() {console.log('Bla bla bla bla...');resolve();}, 1000); }); }
+function hablar(nombre) { return new Promise (( resolve, reject) => { setTimeout(function() {console.log('Bla bla bla bla...');resolve(nombre);}, 1000); }); }
 
 
 
 function adios (nombre) { return new Promise ((resolve, reject) => {setTimeout(function() {console.log('Adios ', nombre); resolve();}, 1000);});} 
-
-
 
 
 
