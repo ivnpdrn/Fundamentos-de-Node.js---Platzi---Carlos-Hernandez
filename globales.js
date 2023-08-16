a48fb7a872b5055d0b1cda2363579f0b9716c070
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-16 16:24:35 -0400

    setInterval
    Hola
    Hola
    Hola
    Hola
    [nodemon] clean exit - waiting for changes before restart

--- globales.js
+++ globales.js
@@ -1,1 +1,8 @@
-console.log(global);
+let i=0;
+let intervalo = setInterval(function()   {
+    console.log('Hola');
+    if (i === 3)   {
+        clearInterval(intervalo);
+    }
+    i++;
+}, 1000);
