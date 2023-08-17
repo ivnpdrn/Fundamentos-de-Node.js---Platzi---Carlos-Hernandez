67817f3392892008b91966f684ecf363ed04d4f4
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-17 17:30:18 -0400

    console.group('Hola'); -
    Conversacion:
      Hola 
        bla
          Blablabla
          Blablabla
          Blablabla
          Adios
        Adios
      otra cosas de otra funcion

--- consola.js
+++ consola.js
@@ -1,10 +1,15 @@
-console.log('Conversacion:');
-console.group('conver');
-console.log('Hola');
+
+
+console.group('Conversacion:');
+console.group('Hola');
+console.group('bla');
+console.log('Blablabla');
+console.log('Blablabla');
 console.log('Blablabla');
 console.log('Adios');
-console.groupEnd('conver');
-
+console.groupEnd('bla');
+console.log('Adios');
+console.groupEnd('Conversacion');
 console.log('otra cosas de otra funcion')
 
 
