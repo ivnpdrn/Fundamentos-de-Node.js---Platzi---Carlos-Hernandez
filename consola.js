b4a86896fed13618a8a60ef4ebb1dbafebb354be
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-17 18:04:50 -0400

    console.count('veces');
    veces: 1
    veces: 2
    veces: 3
    veces: 4
    veces: 5

--- consola.js
+++ consola.js
@@ -1,27 +1,12 @@
-function function1()  {
-    console.group('function 1');
-    console.log('Esto es de la funcion1');
-    console.log('Esto tambien');
-    console.log('Esto tambien');
-    function2();
-    console.log('Hemos vuelto a la 1');
-    console.groupEnd('function 1');
-
-
-}
-
-function function2()  {
-    console.group('function 2');
-    console.log('ahora estamos en la funcion 2');
-    console.groupEnd('function 2');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
 
 
 
-}
 
-
-console.log('empezamos');
-function1();
 
 
 
