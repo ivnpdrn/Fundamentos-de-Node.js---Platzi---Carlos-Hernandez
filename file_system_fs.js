d6dfc3a3a49c5d6716cd0de6089f9ea43e6a3d2a
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-16 23:03:32 -0400

    fs.writeFile(ruta, contenido, funcion (err)
    Se ha escrito correctamente
    [nodemon] clean exit - waiting for changes before restart

--- file_system_fs.js
+++ file_system_fs.js
@@ -3,11 +3,20 @@ const fs = require('fs');
 function leer (ruta, cb) {
     fs.readFile(ruta, (err, data) =>  {             
         cb(data.toString());
-
-
 })
 }
 
-//leer(__dirname + '/archivo.txt')
-leer('archivo.txt',console.log)  
+function escribir(ruta, contenido, cb)  {
+    fs.writeFile(ruta, contenido, function (err)  {
+        if (err) {
+            console.error('No he podido escribirlo', err);
+        } else {
+            console.log('Se ha escrito correctamente');
+        }
+    });
+}
+
+//leer('archivo.txt',console.log)
+
+escribir('archivo1.txt', 'Soy un archivo nuevo', console.log);
 
