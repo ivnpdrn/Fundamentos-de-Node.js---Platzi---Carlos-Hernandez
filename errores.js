d0d36252376405093ac1c46b4eb167646fb4a9e4
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-18 15:57:08 -0400

    console.error(err); cuentamelo
    Vaya, algo se ha roto...
    ReferenceError: z is not defined
        at serompe (C:\Users\13053\OneDrive\documents\Node.js\Fundamentos-Node-Platzi\modulos\errores.js:2:16)
        at Object.<anonymous> (C:\Users\13053\OneDrive\documents\Node.js\Fundamentos-Node-Platzi\modulos\errores.js:6:5)
        at Module._compile (node:internal/modules/cjs/loader:1256:14)
        at Module._extensions..js (node:internal/modules/cjs/loader:1310:10)
        at Module.load (node:internal/modules/cjs/loader:1119:32)
        at Module._load (node:internal/modules/cjs/loader:960:12)
        at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
        at node:internal/main/run_main_module:23:47
    esto de aqui esta al final

--- errores.js
+++ errores.js
@@ -2,14 +2,15 @@ function serompe()  {
     return 3 + z;
 }
 
-// try {
+try {
     serompe();
-// }
+}
 
-// catch(err) {
- //   console.error('Vaya, algo se ha roto...');
-// }
+catch(err) {
+   console.error('Vaya, algo se ha roto...');
+   console.error(err);  // cuentame lo que ha pasado
+ }
 
 console.log('esto de aqui esta al final');
 
-// no llega hasta esta ultima instruccion porque se rompe antes
+// no llega hasta esta ultima instruccion porque se rompe antes si no tiene el try/catch
