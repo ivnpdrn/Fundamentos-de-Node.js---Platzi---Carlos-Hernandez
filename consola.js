06ef6631380a3dac28a092d3b96d1b2f14e3eae0
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-18 10:58:15 -0400

    function1 in function2 structure
    empezamos
    function 1
      Esto es de la funcion 1        
      Esto tambien
      Esto tambien
      function 2
        ahora estamos en la funcion 2
      hemos vuelto a la funcion 1

--- consola.js
+++ consola.js
@@ -1,8 +1,30 @@
-console.count('veces');
-console.count('veces');
-console.count('veces');
-console.countReset('veces');
-console.count('veces');
+// console.count('veces');
+// console.count('veces');
+// console.count('veces');
+// console.countReset('veces');
+// console.count('veces');
+
+function function1() {
+    console.group('function 1');
+    console.log('Esto es de la funcion 1')
+    console.log('Esto tambien');
+    console.log('Esto tambien');
+    function2();
+    console.log('hemos vuelto a la funcion 1');
+    console.groupEnd('function 1'); 
+} 
+
+function function2()  {
+    console.group('function 2');
+    console.log('ahora estamos en la funcion 2');
+    console.groupEnd('function 2');
+}
+
+console.log('empezamos');
+function1();
+
+
+
 
 
 
