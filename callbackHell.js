223bd9393ad86d249c48d19527ed9d4a90b64354
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-19 11:46:29 -0400

    Update callbackHell.js
    Certainly, I'll break down how each callback works in this updated script:
    
    javascript
    Copy code
    function hola(nombre, miCallback) {
        setTimeout(function() {
            console.log('Hola, ' + nombre);
            miCallback(nombre);
        }, 1500);
    }
    
    function hablar(callbackHablar) {
        setTimeout(function() {
            console.log('Bla bla bla bla...');
            callbackHablar();
        });
    }
    
    function adios(nombre, otroCallback) {
        setTimeout(function() {
            console.log('Adios, ' + nombre);
            otroCallback();
        }, 1000);
    }
    
    function conversacion(nombre, veces, callback) {
        if (veces > 0) {
            hablar(function() {
                conversacion(nombre, --veces, callback);
            });
        } else {
            adios(nombre, callback);
        }
    }
    
    console.log('Iniciando proceso...');
    hola('Carlos', function(nombre) {
        conversacion(nombre, 3, function() {
            console.log('Proceso terminado');
        });
    });
    In this script:
    
    hola Function:
    
    The hola function takes nombre and miCallback as arguments.
    After a delay of 1500ms, it logs "Hola, [nombre]".
    Then, it calls the provided miCallback function and passes nombre as an argument.
    hablar Function:
    
    The hablar function takes callbackHablar as an argument.
    After a delay (no specific time mentioned), it logs "Bla bla bla bla...".
    Then, it calls the provided callbackHablar function.
    adios Function:
    
    The adios function takes nombre and otroCallback as arguments.
    After a delay of 1000ms, it logs "Adios, [nombre]".
    Then, it calls the provided otroCallback function.
    conversacion Function:
    
    The conversacion function takes nombre, veces, and callback as arguments.
    If veces is greater than 0, it repeatedly calls the hablar function and then recursively calls itself with veces decreased by 1. This simulates a conversation with "hablar" occurring veces times.
    If veces is 0 or less, it calls the adios function.
    Main Execution:
    
    The script starts by logging "Iniciando proceso..." to the console.
    It then calls the hola function, which logs "Hola, Carlos" and triggers the conversacion function.
    The conversacion function simulates a conversation with multiple calls to the hablar function and ends with the adios function.
    Finally, after the conversation is complete, the script logs "Proceso terminado" to the console.
    This script demonstrates a series of callbacks to simulate a conversation-like sequence, where messages are logged at specific intervals using asynchronous setTimeout.

--- callbackHell.js
+++ callbackHell.js
@@ -22,3 +22,78 @@ function conversacion(nombre, veces, callback) {if (veces > 0) {hablar(function 
 // last active line:    hola('Carlos', function(nombre) {adios(nombre,function() { console.log('Terminamos');});});
 
     // hola('Carlos', function(nombre) { hablar(function() { hablar(function() { hablar(function() { adios(nombre, function() { console.log('Terminando proceso...');});});});});});
+
+// FROM ChatGPT:
+/* 
+
+Certainly, I'll break down how each callback works in this updated script:
+
+javascript
+Copy code
+function hola(nombre, miCallback) {
+    setTimeout(function() {
+        console.log('Hola, ' + nombre);
+        miCallback(nombre);
+    }, 1500);
+}
+
+function hablar(callbackHablar) {
+    setTimeout(function() {
+        console.log('Bla bla bla bla...');
+        callbackHablar();
+    });
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Adios, ' + nombre);
+        otroCallback();
+    }, 1000);
+}
+
+function conversacion(nombre, veces, callback) {
+    if (veces > 0) {
+        hablar(function() {
+            conversacion(nombre, --veces, callback);
+        });
+    } else {
+        adios(nombre, callback);
+    }
+}
+
+console.log('Iniciando proceso...');
+hola('Carlos', function(nombre) {
+    conversacion(nombre, 3, function() {
+        console.log('Proceso terminado');
+    });
+});
+In this script:
+
+hola Function:
+
+The hola function takes nombre and miCallback as arguments.
+After a delay of 1500ms, it logs "Hola, [nombre]".
+Then, it calls the provided miCallback function and passes nombre as an argument.
+hablar Function:
+
+The hablar function takes callbackHablar as an argument.
+After a delay (no specific time mentioned), it logs "Bla bla bla bla...".
+Then, it calls the provided callbackHablar function.
+adios Function:
+
+The adios function takes nombre and otroCallback as arguments.
+After a delay of 1000ms, it logs "Adios, [nombre]".
+Then, it calls the provided otroCallback function.
+conversacion Function:
+
+The conversacion function takes nombre, veces, and callback as arguments.
+If veces is greater than 0, it repeatedly calls the hablar function and then recursively calls itself with veces decreased by 1. This simulates a conversation with "hablar" occurring veces times.
+If veces is 0 or less, it calls the adios function.
+Main Execution:
+
+The script starts by logging "Iniciando proceso..." to the console.
+It then calls the hola function, which logs "Hola, Carlos" and triggers the conversacion function.
+The conversacion function simulates a conversation with multiple calls to the hablar function and ends with the adios function.
+Finally, after the conversation is complete, the script logs "Proceso terminado" to the console.
+This script demonstrates a series of callbacks to simulate a conversation-like sequence, where messages are logged at specific intervals using asynchronous setTimeout.
+*/
