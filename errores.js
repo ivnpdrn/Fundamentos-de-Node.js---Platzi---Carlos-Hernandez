4e257bf716c4409c289a0cb6f51b666ac722f440
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-18 21:39:02 -0400

    procesos asincronos

--- errores.js
+++ errores.js
@@ -10,29 +10,13 @@ function serompe()  {
     return 3 + z;
 }
 
-function seRompeAsincrona(cb) {
-    setTimeout(function()   {    
-        try {
-            return 3 + z;
-        } catch (err){
-            console.error('Error en mi funcion asincrona');
-            cb(err);
-        }
+function seRompeAsincrona(cb) { setTimeout(function()  {  
+    try { return 3 + z;} 
+    catch (err){console.error('Error en mi funcion asincrona'); cb(err);}})} // llega hasta aca y callback(err) porque falla el try
 
-    })
-}
+try {seRompeAsincrona(function(err) { console.log("finaliza aca " + err.message)});}  // todo se inica aca y termina aca por el callback por err
+catch(err) {console.error('Vaya, algo se ha roto...'); console.error(err); console.log('Pero no pasa nada, lo hemos capturado');}
 
-try {
-    //otraFunction();
-    seRompeAsincrona(function(err) { console.log(err.message)});
-}
+console.log('esto de aqui esta al final y se ejecuta despues de todo pero como es asincronico se muestra al principio porque lo demas tarda aunque empiezan primero');
 
-catch(err) {
-   console.error('Vaya, algo se ha roto...');
-   console.error(err);  // cuentame lo que ha pasado, ahora mejor resumido
-   console.log('Pero no pasa nada, lo hemos capturado');
- }
 
-console.log('esto de aqui esta al final');
-
-// no llega hasta esta ultima instruccion porque se rompe antes si no tiene el try/catch
