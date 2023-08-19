9c6a909a40098ac0d6186643058bf1df6d10e0d0
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-18 22:31:45 -0400

    const {exec} = require ('child_process');
    // child_process module is used to execute a shell commmand, 'dir' in this case, and capture its output.
    // the function takes a callback as its second argument.This callback will be executed once the command execution is complete.
    // Inside the callback, the code checks if there's an error ('err') teturned for the 'exec' function. 
    // if there an error, it's printed to the console using 'console.error()' 
    // if there is not error, the script prints the standard output(stdout) of the executed commandto the console using 'console.log' 
    // The 'stdout' variable contains the output of the shell command, which in this case would in this case would be the list of files
    // and directories in the current directory.

---
+++ procesoHijo.js
@@ -0,0 +1,20 @@
+
+const { exec } = require('child_process');
+
+exec('dir', (err, stdout, sterr) => {
+    if (err)  {
+        console.error(err);
+        return false;
+    }
+
+    console.log(stdout);
+
+})
+
+// child_process module is used to execute a shell commmand, 'dir' in this case, and capture its output.
+// the function takes a callback as its second argument.This callback will be executed once the command execution is complete.
+// Inside the callback, the code checks if there's an error ('err') teturned for the 'exec' function. 
+// if there an error, it's printed to the console using 'console.error()' 
+// if there is not error, the script prints the standard output(stdout) of the executed commandto the console using 'console.log' 
+// The 'stdout' variable contains the output of the shell command, which in this case would in this case would be the list of files
+// and directories in the current directory. 
