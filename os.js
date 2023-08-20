1720468e035b2071326d25401d8601449c54ca9b
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-20 17:38:44 -0400

    onsole.log(os.freemem());
    [nodemon] starting `node os.js`
    1177427968
    [nodemon] clean exit - waiting for changes before restart

--- os.js
+++ os.js
@@ -6,6 +6,7 @@ const os = require('os');
 // console.log(os.arch());
 // console.log(os.platform());
 // console.log(os.cpus().length);
+// console.log(os.constants);
 
-console.log(os.constants);
+console.log(os.freemem());
 
