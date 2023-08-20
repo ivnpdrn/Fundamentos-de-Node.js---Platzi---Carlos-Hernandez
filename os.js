b23ea93de2c5ce1389b86f05822de2a9af358720
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-20 18:23:52 -0400

    console.log(gb(os.totalmem()));
    7.368656158447266
    [nodemon] clean exit - waiting for changes before restart

--- os.js
+++ os.js
@@ -8,12 +8,15 @@ const os = require('os');
 // console.log(os.cpus().length);
 // console.log(os.constants);
 
-const SIZE = 1024;
-function kb(bytes) { return bytes / SIZE }
-function mb(bytes) { return kb(bytes) / SIZE }
-function gb(bytes) { return mb(bytes) / SIZE }
+   const SIZE = 1024;
+   function kb(bytes) { return bytes / SIZE }
+   function mb(bytes) { return kb(bytes) / SIZE }
 
-console.log(os.freemem());
-console.log(kb(os.freemem()));
-console.log(mb(os.freemem()));
-console.log(gb(os.freemem()));
+   function gb(bytes) { return mb(bytes) / SIZE }
+
+// console.log(os.freemem());
+// console.log(kb(os.freemem()));
+// console.log(mb(os.freemem()));
+// console.log(gb(os.freemem()));
+
+console.log(gb(os.totalmem()));
